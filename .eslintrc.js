ae1dda8f33353410c3e985c4a9ad29caa877455a
Muhammad Azhar <xzmazhar@rakbank.ae>
2023-05-06 17:41:36 +0400

    Missing import and lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-hooks/rules-of-hooks': 'off', // Checks rules of Hooks
+    'react/no-unstable-nested-components': 'off',
+  },
 };
